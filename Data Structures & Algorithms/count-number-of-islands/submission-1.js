959f7b06e3042dbd63a70d17b11d15339413b5a0
Snehashree Hazra <snehashreehazra783@gmail.com>
2026-07-23 22:29:27 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    numIslands(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+        let count = 0;
+
+        const dfs = (r, c) => {
+            if (r < 0 || r >= rows || c < 0 || c >= cols) return;
+            if (grid[r][c] === "0") return;
+
+            grid[r][c] = "0"; // sink the land
+            dfs(r + 1, c);
+            dfs(r - 1, c);
+            dfs(r, c + 1);
+            dfs(r, c - 1);
+        };
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === "1") {
+                    count++;
+                    dfs(r, c);
+                }
+            }
+        }
+
+        return count;
+    }
+}
